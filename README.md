1d62e5e4daf55c1a9aba3532e193d5b2cd1fb220
alecferguson <160795559+alecferguson@users.noreply.github.com>
2024-10-23 14:51:07 -0700

    Setting up project

--- README.md
+++
@@ -1,2 +0,0 @@
-# Chatter-Box
-Chatterbox is a social networking platform that enables users to create posts and stay informed about the activities and updates of their connections.
---
+++ backend/server.js
@@ -0,0 +1,12 @@
+//creating express server
+import express from "express"
+
+const app = express()
+const PORT = 8000
+app.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`)
+} )
+
+app.get("/",(req,res) => {
+    res.send("Hello")
+})
