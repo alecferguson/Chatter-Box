eab3670a9fa12a9b3f5413d5357dc917ff62a45b
alecferguson <160795559+alecferguson@users.noreply.github.com>
2024-11-07 13:33:32 -0800

    connected to mongoDB

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+//creating express server & env
+import express from "express"
+import dotenv from "dotenv"
+//importing routes
+import authRoutes from "./routes/auth.routes.js"
+import connectMongoDB from "./db/connectMongoDB.js"
+dotenv.config(); //Call this function to enable .env
+const app = express()
+const PORT = process.env.PORT || 8000
+
+//AUTH Route
+app.use("/api/auth",authRoutes)
+
+console.log(process.env.MONGO_URI)
+app.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`)
+    connectMongoDB();
+})
