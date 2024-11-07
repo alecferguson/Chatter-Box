3325f21d52c3a8ce9982928f178eba98fb30593f
alecferguson <160795559+alecferguson@users.noreply.github.com>
2024-11-06 16:25:41 -0800

    created controllers & routers

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 //creating express server
 import express from "express"
-
+//importing routes
+import authRoutes from "./routes/auth.routes.js"
 const app = express()
 const PORT = 8000
+
+//App is 
+app.use("/api/auth",authRoutes)
+
 app.listen(PORT, () =>{
     console.log(`Server is running on port ${PORT}`)
-} )
-
-app.get("/",(req,res) => {
-    res.send("Hello")
 })
