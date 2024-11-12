c220d7270549034b07ca4bfb2f9205dad246f10a
Emmanuel <emrod723@gmail.com>
2024-11-11 22:09:10 -0800

    1) Auth Routes & Controllers

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,18 @@
 //creating express server
-import express from "express"
+import express from 'express';
+import dotenv from 'dotenv';
+import {authRoutes } from './routes/auth.routes.js';
+import connectMongoDB from './db/connectMongodb.js';
 
-const app = express()
-const PORT = 8000
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+console.log(process.env.MONGO_URI);
+
+app.use("/api/auth", authRoutes); 
+
 app.listen(PORT, () =>{
-    console.log(`Server is running on port ${PORT}`)
+    console.log(`Server is running on port ${PORT}`);
+    connectMongoDB();
 } )
-
-app.get("/",(req,res) => {
-    res.send("Hello")
-})
