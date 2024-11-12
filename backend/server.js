64cb733c9159d944ce9e1c7565502010776fd3cc
Emmanuel <emrod723@gmail.com>
2024-11-12 00:00:01 -0800

    1) 49:31 Postman extension

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-console.log(process.env.MONGO_URI);
+app.use(express.json()); // for parsing req.body
 
-app.use("/api/auth", authRoutes); 
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () =>{
     console.log(`Server is running on port ${PORT}`);
