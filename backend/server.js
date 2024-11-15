ffd35b008311f15b0221ea4777653706f7fd2bf7
Clara <145510009+clararity@users.noreply.github.com>
2024-11-14 22:07:26 -0800

    1st - Post Routes & Controllers Complete

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
-import cookieParse from "cookie-parser";
+import cookieParser from "cookie-parser";
 import {v2 as cloudinary} from "cloudinary";
+
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import postRoutes from "./routes/post.routes.js";
 
 import connectMongoDB from "./db/connectMongoDB.js";
 
@@ -21,10 +23,11 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json()); // to parse req.body
 app.use(express.urlencoded({ extended: true })); // to parse form data (urlencoded)
 
-app.use(cookieParse());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
