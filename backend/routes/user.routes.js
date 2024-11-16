0b391d3af46473e44dc9c97e568a69b4c9f8e5cb
Alec Ferguson <160795559+alecferguson@users.noreply.github.com>
2024-11-15 16:14:21 -0800

    Update user.routes.js

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,7 +2,7 @@ import express from 'express'
 import { protectRoute } from '../middleware/protectRoute.js'
 import { getUserProfile } from '../controllers/user.controller.js'
 const router = express.Router()
-router.get("/profiles/:username", protectRoute, getUserProfile) //:username is dynamic
+router.get("/profile/:username", protectRoute, getUserProfile) //:username is dynamic
 //router.get("/suggested", , protectRoute, getUserProfile)
 //router.post("/follow/:id", protectRoute, followUnfollowUser)
 //router.post("/update", protectRoute, updateUserProfile)
