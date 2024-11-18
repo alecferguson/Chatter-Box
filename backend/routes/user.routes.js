fa262448318e37b7ae58ba0d4c6605aaa040abc7
alecferguson <160795559+alecferguson@users.noreply.github.com>
2024-11-17 19:03:42 -0800

    adding update user functions

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,9 +1,9 @@
 import express from 'express'
 import { protectRoute } from '../middleware/protectRoute.js'
-import { getSuggestedUsers, getUserProfile, followUnfollowUser } from '../controllers/user.controller.js'
+import { updateUser, getSuggestedUsers, getUserProfile, followUnfollowUser } from '../controllers/user.controller.js'
 const router = express.Router()
 router.get("/profile/:username", protectRoute, getUserProfile) //:username is dynamic
 router.get("/suggested", protectRoute, getSuggestedUsers)
 router.post("/follow/:id", protectRoute, followUnfollowUser)
-//router.post("/update", protectRoute, updateUserProfile)
+router.post("/update", protectRoute, updateUser)
 export default router
