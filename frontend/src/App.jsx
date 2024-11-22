c1ea70bd5b5a5579bf93342984aff02ca2e30f71
Alec Ferguson <160795559+alecferguson@users.noreply.github.com>
2024-11-22 15:56:32 -0800

    frontend 1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home/HomePage.jsx"
+import LoginPage from "./pages/auth/login/LoginPage.jsx"
+import SignUpPage from "./pages/auth/signup/SignUpPage.jsx"
+
 function App() {
-  return (
-    <>
-    <button className="btn">Button</button>
-    <button className="btn btn-neutral">Neutral</button>
-    <button className="btn btn-primary">Primary</button>
-    <button className="btn btn-secondary">Secondary</button>
-    <button className="btn btn-accent">Accent</button>
-    <button className="btn btn-ghost">Ghost</button>
-    <button className="btn btn-link">Link</button>
-</>
-  )
+	return (
+		<div className='flex max-w-6xl mx-auto'>
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/signup' element={<SignUpPage />} />
+				<Route path='/login' element={<LoginPage />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,13 +1,23 @@
+import daisyui from "daisyui";
+import daisyUIThemes from "daisyui/src/theming/themes";
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: [
-	  "./index.html",
-	  "./src/**/*.{js,ts,jsx,tsx}",
-	],
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-	  extend: {},
+		extend: {},
 	},
-	plugins: [
-    require('daisyui'),
-  ],
-  }
+	plugins: [daisyui],
+
+	daisyui: {
+		themes: [
+			"light",
+			{
+				dark: {
+					...daisyUIThemes["dark"],
+					primary: "#0ea5e9",
+					secondary: "rgb(24, 24, 24)",
+				},
+			},
+		],
+	},
+};
