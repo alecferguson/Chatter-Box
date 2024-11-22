05ccee0b7910ae07ea140e0bcea60fa60751d81b
Alec Ferguson <160795559+alecferguson@users.noreply.github.com>
2024-11-22 13:43:38 -0800

    finished post controller

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -39,14 +39,15 @@ export const createPost = async (req, res) => {
 export const deletePost = async (req, res) => {
 	try {
 		const post = await Post.findById(req.params.id)
+        // No post found
 		if (!post) {
 			return res.status(404).json({ error: "Post not found" })
 		}
-
+        // Are we the owner
 		if (post.user.toString() !== req.user._id.toString()) {
 			return res.status(401).json({ error: "You are not authorized to delete this post" })
 		}
-
+        // If post has image delete it
 		if (post.img) {
 			const imgId = post.img.split("/").pop().split(".")[0]
 			await cloudinary.uploader.destroy(imgId)
@@ -75,12 +76,9 @@ export const commentOnPost = async (req, res) => {
 		if (!post) {
 			return res.status(404).json({ error: "Post not found" })
 		}
-
 		const comment = { user: userId, text }
-
 		post.comments.push(comment);
 		await post.save();
-
 		res.status(200).json(post);
 	} catch (error) {
 		console.log("Error in commentOnPost controller: ", error)
@@ -92,24 +90,20 @@ export const likeUnlikePost = async (req, res) => {
 	try {
 		const userId = req.user._id
 		const { id: postId } = req.params
-
-		const post = await Post.findById(postId);
+		const post = await Post.findById(postId)
 
 		if (!post) {
 			return res.status(404).json({ error: "Post not found" })
 		}
 
 		const userLikedPost = post.likes.includes(userId)
-
 		if (userLikedPost) {
-			// Unlike post
 			await Post.updateOne({ _id: postId }, { $pull: { likes: userId } })
 			await User.updateOne({ _id: userId }, { $pull: { likedPosts: postId } })
 
 			const updatedLikes = post.likes.filter((id) => id.toString() !== userId.toString())
 			res.status(200).json(updatedLikes)
 		} else {
-			// Like post
 			post.likes.push(userId);
 			await User.updateOne({ _id: userId }, { $push: { likedPosts: postId } })
 			await post.save();
@@ -118,7 +112,7 @@ export const likeUnlikePost = async (req, res) => {
 				from: userId,
 				to: post.user,
 				type: "like",
-			});
+			})
 			await notification.save()
 
 			const updatedLikes = post.likes
@@ -141,7 +135,7 @@ export const getAllPosts = async (req, res) => {
 			.populate({
 				path: "comments.user",
 				select: "-password",
-			});
+			})
 
 		if (posts.length === 0) {
 			return res.status(200).json([])
