59d32ebfc56d405b062352a4e76fd448c36f676b
Alec Ferguson <160795559+alecferguson@users.noreply.github.com>
2024-11-22 13:48:21 -0800

    fixing routes

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,9 @@ import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 import {v2 as cloudinary} from "cloudinary"
 // Importing routes
-import authRoutes from "./routes/auth.routes.js"
-import userRoutes from "./routes/user.routes.js"
-import postRoutes from "./routes/post.routes.js"
+import authRoutes from "./routes/auth.route.js"
+import userRoutes from "./routes/user.route.js"
+import postRoutes from "./routes/post.route.js"
 // Mongo DB Connection
 import connectMongoDB from "./db/connectMongoDB.js"
 // Enabling ENV & configs
