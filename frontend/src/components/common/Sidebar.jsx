11ab27b6bfa7a62eaa7b87d56f7126333f475578
alecferguson <160795559+alecferguson@users.noreply.github.com>
2024-11-25 07:46:24 -0800

    implemented user interface

--- frontend/src/components/common/Sidebar.jsx
+++ frontend/src/components/common/Sidebar.jsx
@@ -14,57 +14,57 @@ const Sidebar = () => {
 	};
 
 	return (
-		<div className="md:flex-[1_1_0] w-14 max-w-40">
-			<div className="sticky top-0 left-0 h-screen flex flex-col border-r border-gray-700 w-14 md:w-40">
-				<Link to="/" className="flex justify-center md:justify-start">
-                    <XSvg className="w-10 h-10 rounded-full fill-white hover:bg-stone-900" />
+		<div className='md:flex-[2_2_0] w-18 max-w-52'>
+			<div className='sticky top-0 left-0 h-screen flex flex-col border-r border-gray-700 w-20 md:w-full'>
+				<Link to='/' className='flex justify-center md:justify-start'>
+                <XSvg className="w-12 h-12 rounded-full fill-white hover:bg-stone-900" />
 				</Link>
-				<ul className="flex flex-col gap-2 mt-4">
-					<li className="flex justify-center md:justify-start">
+				<ul className='flex flex-col gap-3 mt-4'>
+					<li className='flex justify-center md:justify-start'>
 						<Link
-							to="/"
-							className="flex gap-2 items-center hover:bg-stone-900 transition-all rounded-full duration-300 py-1.5 pl-2 pr-3 max-w-fit cursor-pointer"
+							to='/'
+							className='flex gap-3 items-center hover:bg-stone-900 transition-all rounded-full duration-300 py-2 pl-2 pr-4 max-w-fit cursor-pointer'
 						>
-							<MdHomeFilled className="w-6 h-6" />
-							<span className="text-sm hidden md:block">Home</span>
+							<MdHomeFilled className='w-8 h-8' />
+							<span className='text-lg hidden md:block'>Home</span>
 						</Link>
 					</li>
-					<li className="flex justify-center md:justify-start">
+					<li className='flex justify-center md:justify-start'>
 						<Link
-							to="/notifications"
-							className="flex gap-2 items-center hover:bg-stone-900 transition-all rounded-full duration-300 py-1.5 pl-2 pr-3 max-w-fit cursor-pointer"
+							to='/notifications'
+							className='flex gap-3 items-center hover:bg-stone-900 transition-all rounded-full duration-300 py-2 pl-2 pr-4 max-w-fit cursor-pointer'
 						>
-							<IoNotifications className="w-5 h-5" />
-							<span className="text-sm hidden md:block">Notifications</span>
+							<IoNotifications className='w-6 h-6' />
+							<span className='text-lg hidden md:block'>Notifications</span>
 						</Link>
 					</li>
 
-					<li className="flex justify-center md:justify-start">
+					<li className='flex justify-center md:justify-start'>
 						<Link
 							to={`/profile/${data?.username}`}
-							className="flex gap-2 items-center hover:bg-stone-900 transition-all rounded-full duration-300 py-1.5 pl-2 pr-3 max-w-fit cursor-pointer"
+							className='flex gap-3 items-center hover:bg-stone-900 transition-all rounded-full duration-300 py-2 pl-2 pr-4 max-w-fit cursor-pointer'
 						>
-							<FaUser className="w-5 h-5" />
-							<span className="text-sm hidden md:block">Profile</span>
+							<FaUser className='w-6 h-6' />
+							<span className='text-lg hidden md:block'>Profile</span>
 						</Link>
 					</li>
 				</ul>
 				{data && (
 					<Link
 						to={`/profile/${data.username}`}
-						className="mt-auto mb-6 flex gap-2 items-center transition-all duration-300 hover:bg-[#181818] py-2 px-3 rounded-full"
+						className='mt-auto mb-10 flex gap-2 items-start transition-all duration-300 hover:bg-[#181818] py-2 px-4 rounded-full'
 					>
-						<div className="avatar hidden md:inline-flex">
-							<div className="w-6 rounded-full">
+						<div className='avatar hidden md:inline-flex'>
+							<div className='w-8 rounded-full'>
 								<img src={data?.profileImg || "/avatar-placeholder.png"} />
 							</div>
 						</div>
-						<div className="flex justify-between flex-1">
-							<div className="hidden md:block">
-								<p className="text-white font-bold text-sm truncate">{data?.fullName}</p>
-								<p className="text-slate-500 text-xs">@{data?.username}</p>
+						<div className='flex justify-between flex-1'>
+							<div className='hidden md:block'>
+								<p className='text-white font-bold text-sm w-20 truncate'>{data?.fullName}</p>
+								<p className='text-slate-500 text-sm'>@{data?.username}</p>
 							</div>
-							<BiLogOut className="w-4 h-4 cursor-pointer" />
+							<BiLogOut className='w-5 h-5 cursor-pointer' />
 						</div>
 					</Link>
 				)}
