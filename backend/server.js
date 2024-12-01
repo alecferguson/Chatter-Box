2d654857b2c89b1c4ffa4659525cb7bcee4d0589
alecferguson <160795559+alecferguson@users.noreply.github.com>
2024-12-01 12:36:23 -0800

    added create a post function

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,38 @@
 // Importing dependencies
-import express from "express"
-import dotenv from "dotenv"
-import cookieParser from "cookie-parser"
-import {v2 as cloudinary} from "cloudinary"
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
 // Importing routes
-import authRoutes from "./routes/auth.route.js"
-import userRoutes from "./routes/user.route.js"
-import postRoutes from "./routes/post.route.js"
-import notificationRoutes from "./routes/notification.route.js"
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
+import notificationRoutes from "./routes/notification.route.js";
 // Mongo DB Connection
-import connectMongoDB from "./db/connectMongoDB.js"
+import connectMongoDB from "./db/connectMongoDB.js";
 // Enabling ENV & configs
-dotenv.config()
+dotenv.config();
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-})
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 // Creating express app and setting port
-const app = express()
-const PORT = process.env.PORT || 8000
+const app = express();
+const PORT = process.env.PORT || 8000;
 
 // Middleware
-app.use(express.json()) // Parse req.body
-app.use(express.urlencoded({ extended: true})) // Used for postman
-app.use(cookieParser()) // Used for protectRoute
+app.use(express.json({ limit: "5mb" })); // Parse req.body
+app.use(express.urlencoded({ extended: true })); // Used for postman
+app.use(cookieParser()); // Used for protectRoute
 // Routes
-app.use("/api/auth", authRoutes)
-app.use("/api/users", userRoutes)
-app.use("/api/posts", postRoutes)
-app.use("/api/notifications", notificationRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
 // Listening for port & connecting to mongoDB
-console.log(process.env.MONGO_URI)
+console.log(process.env.MONGO_URI);
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-    connectMongoDB()
-})
+  console.log(`Server is running on port ${PORT}`);
+  connectMongoDB();
+});
