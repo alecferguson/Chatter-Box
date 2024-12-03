a25e4570179047c6cecf3ce8d50002c084457fc6
ttalampas <124741869+ttalampas@users.noreply.github.com>
2024-12-02 20:21:54 -0800

    auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,32 @@ import ProfilePage from "./pages/profile/ProfilePage.jsx";
 import { Toaster } from "react-hot-toast"; 
 
 function App() {
+  const { data, isLoading, error, isError} = useQuery({
+    queryKey: ['authUser'],
+    queryFn: async() => {
+      try {
+        const res = await fetch("/api/auth/me");
+        const data = await res.json();
+        if(!res.ok) {
+          throw new Error(data.error || "Something went wrong")
+        }
+        console.log("authUser is here:", data); 
+        return data;
+
+      } catch (error) {
+
+      }
+    }
+  });
+
+  if(isLoading) {
+    return (
+      <div className = 'h=screen flex justify-center items-center'>
+        <LoadingSpinner size = 'lg' />
+      </div>
+    )
+  }
+
   return (
     <div className="flex max-w-6xl mx-auto">
       <Sidebar />
@@ -23,6 +49,6 @@ function App() {
       <Toaster />
     </div>
   );
-}
+};
 
 export default App;
